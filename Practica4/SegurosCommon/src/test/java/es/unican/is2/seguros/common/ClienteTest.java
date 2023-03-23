27cc192a7f7e58157c639d70c75915191f273cac
FrozenCureBand <jmarroquinsoto@gmail.com>
2023-03-23 12:28:00 +0100

    Creación Clases Test

---
+++ Practica4/SegurosCommon/src/test/java/es/unican/is2/seguros/common/ClienteTest.java
@@ -0,0 +1,14 @@
+package es.unican.is2.seguros.common;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ClienteTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Practica4/SegurosCommon/src/test/java/es/unican/is2/seguros/common/SeguroTest.java
@@ -0,0 +1,14 @@
+package es.unican.is2.seguros.common;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SeguroTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
