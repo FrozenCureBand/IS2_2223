009d9b58d2b881fb1e61ed56438f7bdca2c133f2
FrozenCureBand <jmarroquinsoto@gmail.com>
2023-06-16 23:05:25 +0200

    Commit

---
+++ Practica5/practica5_refactorizado/src/main/java/es/unican/is2/practica5/Cuenta.java
@@ -0,0 +1,15 @@
+package es.unican.is2.practica5;
+
+public class Cuenta {
+	
+	private String numCuenta;
+	
+	public Cuenta(String numCuenta) {
+		this.numCuenta = numCuenta;
+	}
+	
+	public String getNumCuenta() {
+		return numCuenta;
+	}
+	
+}
